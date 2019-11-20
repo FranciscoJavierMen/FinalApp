ad0a16b6decf06a3126b91f93ebb72d1551a3b9f
Javi_Altatec <francisco.javier.mendoza.licea@gmail.com>
2019-11-19 22:33:11 -0600

    Reset password by email

--- app/src/main/java/com/example/finalapp/actvities/login/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/finalapp/actvities/login/ForgotPasswordActivity.kt
@@ -1,10 +1,18 @@
 package com.example.finalapp.actvities.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.finalapp.R
+import com.example.finalapp.isValidEmail
+import com.example.finalapp.validate
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +23,28 @@ class ForgotPasswordActivity : AppCompatActivity() {
 
     private fun initComponents() {
 
+        edtRestorePasswordLayoutEmail.validate {
+            edtRestorePasswordLayoutEmail.editText?.error = if (isValidEmail(it)) null else "Invalid email"
+        }
+
+        btnGoToLogin.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+
+        btnResetPassword.setOnClickListener{
+            val email = edtRestorePasswordLayoutEmail.editText?.text.toString()
+            if (isValidEmail(email)){
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(this){
+                    Toast.makeText(this, "An email has been sent to you email to reset your password", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+            } else {
+                Toast.makeText(this, "Please make sure the email address is correct", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
