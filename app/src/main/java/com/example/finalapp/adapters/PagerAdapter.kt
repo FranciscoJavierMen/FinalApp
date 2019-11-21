ed4685024e413c608036301c3c913c19560ab6f4
Javi_Altatec <francisco.javier.mendoza.licea@gmail.com>
2019-11-20 21:51:39 -0600

    Creating the PagerAdapter

---
+++ app/src/main/java/com/example/finalapp/adapters/PagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.finalapp.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm){
+
+    private val fragmentList = ArrayList<Fragment>()
+
+    override fun getItem(position: Int) = fragmentList[position]
+
+    override fun getCount() = fragmentList.size
+
+    fun addFragment(fragment: Fragment) = fragmentList.add(fragment)
+}
